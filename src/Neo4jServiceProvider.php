a97f7ecc8ce67832be9cd6d9f254c5df4232a2d9
--global <--global>
2025-02-14 14:13:29 +0530

    Initial commit

---
+++ src/Neo4jServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Neo4jPhp\Neo4jLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class Neo4jServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
