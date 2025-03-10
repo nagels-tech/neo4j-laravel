a7de6787fa1d769aeb98272b9c76e7f1ebcecc50
exaby73 <nabeel@ex3.dev>
2025-03-10 09:27:21 +0530

    chore: Add Laravel version installation script for CI workflows

---
+++ .github/scripts/setup-laravel-env.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -z "$1" ]; then
+    echo "Please specify the Laravel version to install"
+    exit 1
+fi
+
+echo "Installing Laravel version $1"
+
+# This is not required for CI, but it allows to test the script locally
+function cleanup {
+    echo "Cleaning up"
+    mv composer.origin.json composer.json
+}
+
+function install-specified-laravel-version {
+    local laravel_version=$1
+    cp composer.json composer.origin.json
+    rm composer.lock || true
+    rm -Rf vendor || true
+    sed -i 's/\^11.0 || \^12.0/\^'$laravel_version'/g' composer.json
+    composer install
+}
+
+trap cleanup EXIT
+
+install-specified-laravel-version $1 
